1a4ced736379aa6986fa9607db537ccc93d2af0d
onkky <wuttiphat.ler@dome.tu.ac.th>
2026-04-27 00:00:21 +0700

    add like/react discussion API in discussion-react

---
+++ backend/functions/discussion-react/index.js
@@ -0,0 +1,19 @@
+import { createDiscussion } from "../../shared/dynamo.js";
+import { created, parseBody, internalError } from "../../shared/http.js";
+
+export const handler = async (event) => {
+  try {
+    const body = parseBody(event);
+    const { courseId, title, content, author } = body;
+
+    const data = await createDiscussion(courseId, {
+      title,
+      content,
+      author,
+    });
+
+    return created(data);
+  } catch (err) {
+    return internalError(err.message);
+  }
+};
